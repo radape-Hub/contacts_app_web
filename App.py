de1317038914065401b1e04035ec52cd205e4bb9
radape-Hub <geeksoyla@gmail.com>
2022-11-14 19:27:12 -0500

    primera Modificación agregando paginas

--- App.py
+++ App.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 def Index():
     return "hola mundo"
 
+@app.route('/add_contact')
+def add_contact():
+    return 'add contact'
+
+@app.route('/edit')
+def edit_contact():
+    return 'edit contact'
+
+@app.route('/delete')
+def delete_contact():
+    return 'delete contact'
+
+
 
 if __name__== '__main__':
     app.run(port=3000, debug=True)
